9d8a40fd5b8e5a1ecd33d8b76c694c5673142487
MyU <myudev0@gmail.com>
2014-07-12 20:18:22 +0200

    Updated Default Config
    + Added German localization

--- simplereports/lua/autorun/ss_srp_config.lua
+++ simplereports/lua/autorun/ss_srp_config.lua
@@ -11,7 +11,7 @@ lang = "EN" -- Language to use, please define it at the bottom of this file!
 play_sound = true -- play sound if a report arrives?
 report_cooldown = 5 -- how many seconds a user needs to wait to apply another report?
 notification_message = true -- should players receive a message every (notification_time) seconds?
-notification_time = 60
+notification_time = 360
 
 
 PREDEF_REASONS = { -- Reasons for the custom (nonplayer) report
@@ -55,6 +55,39 @@ LOCAL_TEXT["EN"] = {
 	REPORT_TEXT_CUSTOM = "%s wrote an Report with the Reason: %s",
 	
 	WAIT_MSG = "You need to wait " .. report_cooldown .. " Seconds between your reports."
+}
+LOCAL_TEXT["DE"] = {
+
+	NOTIFICATION_MSG = "Du kannst Spieler mit Hilfe von !report Melden, oder Admins eine Nachricht senden!",
+
+	SELECT_TYPE = "Wähle eine Kategorie",
+	TYPE_PLAYER = "Spieler Melden",
+	TYPE_MISC = "Nachricht an Verfügbare Teammitglieder Senden",
+	SELECT_REASON = "Wähle einen Grund",
+	NO_INFORMATION = "Bitte fülle das Feld aus!",
+
+	SELECT_PLAYER = "Wähle einen Spieler aus",
+	DEFAULT_REASON = "Tippe hier einen detailierten Grund ein, Spam oder anderes wird Verwarnt!",
+	REASON_TEXT = "Grund:",
+	INFO_TEXT = "Informationen:",
+	DEFAULT_INFO = "Bitte gebe zusätzliche Informatione an.",
+	UNKNOWN_REASON = "Bitte wähle einen Grund aus, wenn keiner der Gründe passt wähle \"Anderes\"",
+
+	SEND_TEXT = "Senden",
+
+	UNKNOWN_PLAYER = "Spieler ist nicht mehr auf dem Server.",
+
+	NO_REASON = "Bitte schreibe einen Grund!",
+
+	INVALID_CHARS = "Der Grund muss mind. 6 Zeichen enthalten.",
+
+	REPORT_SENT = "Report erfolgreich gesendet, bitte warte auf einen Antwort oder Aktion von einen Teammitglied",
+	TEXT_OTHER = "Anderes",
+
+	REPORT_TEXT_PLAYER = "%s wrote an Report against %s type !reports to open them.",
+	REPORT_TEXT_CUSTOM = "%s wrote an Report with the Reason: %s",
+	
+	WAIT_MSG = "Du musst " .. report_cooldown .. " Sekunden warten."
 }
 
 -- Dont touch
